11d5053e9417a77fe4473f7d8a05cb139cd887f1
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2020-09-29 00:45:41 +0300

    Added task 1 for lesson 2

--- lesson2/src/ru/geekbrains/lesson2/Main.java
+++ lesson2/src/ru/geekbrains/lesson2/Main.java
@@ -2,6 +2,25 @@ package ru.geekbrains.lesson2;
 
 public class Main {
     public static void main(String[] args){
-        
+        // Задание 1 - задать целочисленный массив
+        int[] a = new int[10];
+        a[0] = 1;
+        a[1] = 1;
+        a[2] = 0;
+        a[3] = 0;
+        a[4] = 1;
+        a[5] = 0;
+        a[6] = 1;
+        a[7] = 1;
+        a[8] = 0;
+        a[9] = 0;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] == 1){
+                a[i] = 0;
+            } else {
+                a[i] = 1;
+            }
+            System.out.println(a[i]);
+        }
     }
 }
