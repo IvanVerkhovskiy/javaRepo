96dabfad6c741f06c011e3f811d094b516156723
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2020-10-04 01:26:49 +0300

    Fix task 1 for lesson 3 (added three levels)

--- lesson3/src/ru/geekbrains/lesson3/Main.java
+++ lesson3/src/ru/geekbrains/lesson3/Main.java
@@ -8,51 +8,82 @@ public class Main {
     static int value = 1;
 
     public static void main(String[] args){
-        // Вызов метод, если значение равно 1
+        int level;
+        // Вызов игры, если значение равно 1
         while (value == 1){
-            playGame();
+            System.out.println("Ваша задача угадать число.\nКакой уровень сложности вы хотите выбрать?\n" +
+                    "1. От 0 до 9 (3 попытки)\n" +
+                    "2. От 0 до 50 (5 попыток)\n" +
+                    "3. От 0 до 100 (7 попыток)");
+            level = scanner.nextInt();
+            switch (level){
+                case 1:
+                    if (level == 1){
+                        playGame(9, 3);
+                    }
+                    break;
+                case 2:
+                    if (level == 2){
+                        playGame(50, 5);
+                    }
+                    break;
+                case 3:
+                    if (level == 3){
+                        playGame(100, 7);
+                    }
+                    break;
+                default:
+                    playGame(9, 3);
+            }
         }
         // Если значение равно 0, то выходим
         System.out.println("До свидания!");
     }
 
     // Метод, который вызывает игру
-    public static void playGame(){
-        System.out.println("Ваша задача угадать число.\n" +
-                "Пожалуйста, введите задуманное число:");
-        int step = 3; // уровень
+    public static void playGame(int level, int step){
+        System.out.println("Пожалуйста, введите задуманное число от 0 до " + level + ":");
         // Задумываем число от 0 до 9
-        int random = (int)(Math.random() * 9);
-        while (step != 0){
-                int userNumber = scanner.nextInt(); // Пользователь вводит число
-                if (userNumber == random){
-                    System.out.println("Вы угадали! Задуманное число:" + random);
-                    break;
-                } else if (userNumber > random){
-                    System.out.println("Задуманное число меньше!");
-                } else {
-                    System.out.println("Задуманное число больше!");
-                }
-                step--;
-                System.out.println("У вас осталось " + step + " попытки");
-                if (step == 0){
-                    System.out.println("Увы, но вы проиграли!");
-                }
+        int random = (int)(Math.random() * level);
+//        Проверка на ввод числа
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Вы ввели не число! Пожалуйста, попробуйте снова.");
         }
-        while(true){
-            System.out.println("Вы хотите попробовать ещё раз?\n" +
-                    "1 - Да\n" +
-                    "0 - Нет");
-            if (scanner.hasNextInt()){
-                value = scanner.nextInt();
-                if (value == 1 || value == 0){
-                    break;
-                } else {
-                    System.out.println("Вы ввели не ту команду!");
-                }
-            } else {
-                System.out.println("Вы ввели не число!");
+//        Основная часть игры
+        while (step != 0){
+            int userNumber = scanner.nextInt(); // Пользователь вводит число
+            if (userNumber == random){
+                System.out.println("Вы угадали! Задуманное число:" + random);
                 break;
+            } else if (userNumber > random){
+                System.out.println("Задуманное число меньше!");
+            } else {
+                System.out.println("Задуманное число больше!");
+            }
+            step--;
+            System.out.println("У вас осталось " + step + " попытки");
+            if (step == 0){
+                System.out.println("Увы, но вы проиграли! Задуманное число: " + random);
+            }
+        }
+        newGame(); // Метод, который спрашивает, продолжить игру?
+    }
+
+    public static void newGame(){
+        System.out.println("Вы хотите попробовать ещё раз?\n" +
+                "1 - Да\n" +
+                "0 - Нет");
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Вы ввели не число! Пожалуйста, попробуйте снова.");
+        }
+        while (true){
+            value = scanner.nextInt();
+            if (value == 1 || value == 0){
+                break;
+            } else {
+                System.out.println("Вы ввели не ту команду!");
             }
         }
     }
