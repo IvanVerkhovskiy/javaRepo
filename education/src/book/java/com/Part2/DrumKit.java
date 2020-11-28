5acfbb4b831cfaf07e8d2a176347a8f8616890f6
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2020-11-28 17:00:39 +0300

    Created new branch and task

---
+++ education/src/book/java/com/Part2/DrumKit.java
@@ -0,0 +1,16 @@
+package book.java.com.Part2;
+
+public class DrumKit {
+//    Переменные экземпляра
+    boolean topHat = true;
+    boolean snare = true;
+
+//    Методы
+    void playSnare(){
+        System.out.println("бах бах бах-бах");
+    }
+
+    void playTopHat(){
+        System.out.println("динь динь ди-динь");
+    }
+}
---
+++ education/src/book/java/com/Part2/DrumKitTestDrive.java
@@ -0,0 +1,13 @@
+package book.java.com.Part2;
+
+public class DrumKitTestDrive {
+    public static void main(String[] args){
+        DrumKit d = new DrumKit();
+        d.snare = false;
+        d.playSnare();
+        if (d.snare == true){
+            d.playSnare();
+        }
+        d.playTopHat();
+    }
+}
