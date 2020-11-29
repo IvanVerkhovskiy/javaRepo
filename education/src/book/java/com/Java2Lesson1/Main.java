da21f69f91062c984d701717fd86ddd650f4c643
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2020-11-29 17:56:19 +0300

    h/w - geekBrains

--- education/src/book/java/com/Java2Lesson1/Main.java
+++ education/src/book/java/com/Java2Lesson1/Main.java
@@ -2,6 +2,11 @@ package book.java.com.Java2Lesson1;
 
 public class Main {
     public static void main(String[] args) {
-
+        Cat cat = new Cat ("Васька");
+        Cat cat2 = new Cat("Петька");
+        cat.setAge(-10);
+        cat2.setAge(4);
+        cat.getInformationForCat();
+        cat2.getInformationForCat();
     }
 }
