65fbb42f79eb0e521fc1116443d48e73f1b7fe54
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2020-12-20 11:37:28 +0300

    My education

---
+++ education/src/book/java/com/Java2Lesson2/Exception/MyException.java
@@ -0,0 +1,8 @@
+package book.java.com.Java2Lesson2.Exception;
+
+public class MyException extends Exception {
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
