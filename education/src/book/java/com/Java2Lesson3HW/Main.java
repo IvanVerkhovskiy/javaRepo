2f08dd0cd8e207af6dd4a40b6a6767ebc76f809d
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2021-02-28 22:54:50 +0300

    Edit task 3

--- education/src/book/java/com/Java2Lesson3HW/Main.java
+++ education/src/book/java/com/Java2Lesson3HW/Main.java
@@ -7,6 +7,7 @@ public class Main {
         // Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
         // Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
         // Посчитать сколько раз встречается каждое слово.
+        System.out.println("Задание 1");
         ArrayList<String> arrayList = new ArrayList<>(20);
         arrayList.add("car");
         arrayList.add("car");
@@ -41,6 +42,42 @@ public class Main {
             }
             System.out.println(newArray + " " + "(" + count + ")");
             count = 0;
+        }
+
+        //Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и
+        // телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи.
+        // С помощью метода get() искать номер телефона по фамилии.
+        // Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+        // тогда при запросе такой фамилии должны выводиться все телефоны.
+        System.out.println();
+        System.out.println("Задание 2");
+        HashMap<String, String> peopleDB = new HashMap<>();
+        add(peopleDB);
+        get(peopleDB, "Петров");
+        get(peopleDB, "Кутев");
+        get(peopleDB, "Сушов");
+    }
+
+    // Добавление пользователей
+    public static void add(HashMap<String, String> peopleDB){
+        peopleDB.put("8-999-982-90-22","Петров");
+        peopleDB.put("8-000-121-32-32","Веселов" );
+        peopleDB.put("7-342-564-23-66","Кутев") ;
+        peopleDB.put("8-000-222-33-44","Володин");
+        peopleDB.put("8-999-333-33-00","Петров");
+        peopleDB.put("9-454-454-23-23","Сушов");
+        peopleDB.put("9-345-234-33-44","Луронов");
+        peopleDB.put("8-999-999-99-99","Луронов");
+    }
+
+    // Поиск пользователей
+    public static void get(HashMap<String, String> peopleSearch, String name){
+        String value;
+        for (Map.Entry<String, String> map : peopleSearch.entrySet()) {
+            value = map.getValue();
+            if (value.equals(name)){
+                System.out.println(map.getKey() + " " + map.getValue());
+            }
         }
     }
 }
